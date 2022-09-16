ab36147dba2cafce5820303057467afcf2a6cd8f
dhenriquez5 <dhenriquezturcios@gmail.com>
2022-09-15 22:24:28 -0500

    f2

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -30,7 +30,7 @@ const PokemonNamePage: NextPage<Props> = ({ pokemon }) => {
                             <Text h1 transform='capitalize'>{pokemon.name}</Text>
                         </Card.Header>
                         <Card.Body>
-                            <Text size={30}>Spritess: </Text>
+                            <Text size={30}>Sprites: </Text>
                             <Container direction='row' display='flex' gap={0} wrap='nowrap'> 
                                 <Image src={pokemon.sprites.front_default} alt={pokemon.name} width="100%" height={100} />
                                 <Image src={pokemon.sprites.back_default} alt={pokemon.name} width="100%" height={100} />
